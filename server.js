8ef875b245fbaf58bf7ffb88623fbed6ebe2e28b
Jocelyn Belden <jocelynbelden@Jocelyns-MacBook-Pro.local>
2018-02-24 19:56:32 -1000

    add private messages to users or admin

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const server = net.createServer(client => {
 Welcome ${client.name} to the Network Broadcast News Chatroom.
 Please use the following commands for added chatroom functionality:
 
-'$': change your username
+$ : change your username: '$sample'
+~ : view user list
+/ : private message to ADMIN: '/message'
+@ : private message to user: '@user + message'
 `
   );
 
@@ -53,12 +56,31 @@ Please use the following commands for added chatroom functionality:
           client.write("Error: username unavailable");
         }
         break;
+      case "~":
+        const usernames = clients.map(users => users.name);
+        client.write(usernames.toString());
+        break;
+      case "/":
+        console.log(`PM to ADMIN from ${client.name}: ${data.toString()}`);
+        break;
+      case "@":
+        const username = data
+          .toString()
+          .split(" ")[0]
+          .toString()
+          .slice(1);
+        clients.forEach(user => {
+          if (username === user.name) {
+            user.write(data.toString());
+          }
+        });
+        break;
       default:
         broadcast(`${client.name}: ${data.toString()}`, client);
     }
   });
   client.on("end", () => {
-    console.log("client disconnected");
+    console.log(`${client.name} disconnected`);
     broadcast(client.name + " has left the room", client);
     clients.splice(clients.indexOf(client), 1);
   });
